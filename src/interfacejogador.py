554b968d4f0b4c6229ee7f9a6cd92ad07ae9a366
Margot <margot.mb@outlook.com>
2024-05-08 12:45:29 -0300

    Interface Update

--- src/interfacejogador.py
+++ src/interfacejogador.py
@@ -7,9 +7,25 @@ from dog.dog_actor import DogActor
 
 class InterfaceJogador(DogPlayerInterface):
     def __init__(self):
-        self.main_window = Tk()  # instanciar Tk
-        self.fill_main_window()  # organização e preenchimento da janela
-        self.board = Mesa()  # tratamento do domínio do problema
+        # Instanciar Tk
+        self.main_window = Tk()
+        
+        # Organização e preenchimento da janela  
+        self.fill_main_window()  
+
+        # Tratamento do domínio do problema
+        self.board = Mesa()  
+
+        # Set Player Name
+        player_name = simpledialog.askstring(title="Player identification", prompt="Qual o seu nome?")
+        self.dog_server_interface = DogActor()
+
+        # Dog Response
+        message = self.dog_server_interface.initialize(player_name, self)
+        messagebox.showinfo(message=message)
+
+        # Main Loop
+        self.main_window.mainloop()
 
     
     # Popula a janela Tk com os elementos
@@ -18,4 +34,15 @@ class InterfaceJogador(DogPlayerInterface):
         #self.main_window.iconbitmap("src/images/icon.ico")
         self.main_window.geometry("1280x720")
         self.main_window.resizable(False, False)
-        #self.main_window["bg"] = "gold3"
+        self.main_window["bg"] = "white"
+
+
+        self.table_frame = Frame(self.main_window, padx=100, pady=40, bg="gold3")
+        self.table_frame.grid(row=0, column=0)
+        self.message_frame = Frame(self.main_window, padx=0, pady=10, bg="gold3")
+        self.message_frame.grid(row=1, column=0)
+
+        #self.table_frame = Frame(self.main_window, padx=100, pady=40, bg="gold3")
+        #self.table_frame.grid(row=0, column=0)
+        #self.message_frame = Frame(self.main_window, padx=0, pady=10, bg="gold3")
+        #self.message_frame.grid(row=1, column=0)
