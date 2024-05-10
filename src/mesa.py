f50290ae227b175fc337b8a6e1cc1abe19c0c4d8
Margot <margot.mb@outlook.com>
2024-05-09 22:13:03 -0300

    definindo estados de partida

--- src/mesa.py
+++ src/mesa.py
@@ -1,6 +1,14 @@
 import jogador
 import carta
 
+# 	Mesa: estado_partida
+# 1 - estado inicial
+# 2 - partida finalizada (há vencedor)
+# 3 - seu turno, jogo em andamento e aguardando jogada a ser selecionada (fatiar/trocar missão/comprar carta/completar missão)
+# 4 - seu turno, jogo em andamento e aguardando condições de execução (caso precise selecionar carta)
+# 5 - turno do oponente, jogo em andamento - aguardando jogada
+# 6 - recebeu desistência/abandono, atribui jogador local como vencedor
+
 class Mesa:
     def __init__(self):
         super().__init__()
@@ -8,3 +16,4 @@ class Mesa:
         self.remote_player = jogador.Jogador()
         self.__baralho_fracoes  = []
         self.__baralho_missoes = []
+        self.__estado_partida = 1
