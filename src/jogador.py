08c8dd4660f2e96ac8a2dae0bdd8a5d011c2e41e
Margot <margot.mb@outlook.com>
2024-05-09 22:34:37 -0300

    atributos e init player

--- src/jogador.py
+++ src/jogador.py
@@ -1,7 +1,26 @@
 class Jogador:
     def __init__(self):
         self.__identifier = None
+        self.__nome = None
+        self.__num_jogador = None
         self.__missao = None
         self.__cartas_fracao = []
         self.__pizzas = 16
+        self.__meu_turno = False
+        self.__vencedor = False
+
+    def inicializar(self, a_number, an_id, a_name):
+        self.reset()
+        self.__num_jogador = a_number  # int
+        self.__identifier = an_id  #   string
+        self.__name = a_name  #   string
+
+    def reset(self):
+        self.__identifier = None
+        self.__nome = None
+        self.__num_jogador = None
+        self.__missao = None
+        self.__cartas_fracao = []
+        self.__pizzas = 16
+        self.__meu_turno = False
         self.__vencedor = False
