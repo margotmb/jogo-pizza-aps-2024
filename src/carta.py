9a820dda31c93472d0930dea9b26fd64df41723a
Margot <margot.mb@outlook.com>
2024-05-22 01:09:19 -0300

    Update Carta

--- src/carta.py
+++ src/carta.py
@@ -1,5 +1,8 @@
+# valor é definido como quantos 1/8 de pizza a carta vale
+# 4 = 4*1/8 = 1/2, metade de uma pizza de 8 pedaços
+
 class Carta:
-    def __init__(self, cardimage: str, valor: int):
+    def __init__(self, cardimage = None, valor = None):
         self.__cardimage = cardimage
         self.__valor = valor
     
